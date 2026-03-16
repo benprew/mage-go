8577edb3b2186a43e345f1d030b78e271ea304bf
Ben Prew <ben@throwingbones.com>
2026-03-15 23:02:27 -0700

    feat: add auras and X spells to decks for testing

--- internal/tui/decks.go
+++ internal/tui/decks.go
@@ -3,8 +3,8 @@ package tui
 import (
 	"math/rand"
 
-	"github.com/google/uuid"
 	"git.sr.ht/~cdcarter/mage-go/pkg/mage"
+	"github.com/google/uuid"
 )
 
 // DeckEntry defines a card name and count.
@@ -19,7 +19,7 @@ type DeckArchetype struct {
 	Entries []DeckEntry
 }
 
-// humanDeckEntries: White/Green aggro with Healing Salve as a key modal spell.
+// humanDeckEntries: White/Green aggro with auras and combat tricks.
 var humanDeckEntries = []DeckEntry{
 	{"Plains", 9},
 	{"Forest", 7},
@@ -28,21 +28,27 @@ var humanDeckEntries = []DeckEntry{
 	{"Llanowar Elves", 4},
 	{"Grizzly Bears", 3},
 	{"Serra Angel", 2},
-	{"Healing Salve", 4},
+	{"Healing Salve", 2},
 	{"Giant Growth", 2},
 	{"Swords to Plowshares", 2},
+	{"Holy Strength", 2},
+	{"Lure", 1},
+	{"Crusade", 1},
 }
 
-// aiDeckEntries: Red/Black aggro with burn and removal.
+// aiDeckEntries: Red/Black aggro with burn, removal, auras, and X spell.
 var aiDeckEntries = []DeckEntry{
 	{"Mountain", 8},
 	{"Swamp", 8},
 	{"Lightning Bolt", 4},
-	{"Terror", 4},
+	{"Terror", 3},
 	{"Black Knight", 4},
-	{"Ironclaw Orcs", 4},
-	{"Hill Giant", 4},
-	{"Hypnotic Specter", 4},
+	{"Ironclaw Orcs", 3},
+	{"Hill Giant", 3},
+	{"Hypnotic Specter", 3},
+	{"Unholy Strength", 1},
+	{"Firebreathing", 1},
+	{"Disintegrate", 2},
 }
 
 // Archetypes is the list of deck archetypes available in the lobby.
